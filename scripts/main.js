4228d48ad975bb1e4f62fb3fe46648e165e4c015
omar star <obarakat12@gmail.com>
2022-04-11 13:02:52 +0300

    refactor(main.js): categories and shortnames

--- scripts/main.js
+++ scripts/main.js
@@ -2,20 +2,51 @@ const url = "http://filltext.com/?rows=10&fname={firstName}&lname={lastName}&cat
 fetch(url).then(res=> res.json())
 .then(data=>appendData(data)).catch(err=>console.log('err: ',err));
 
+function getFirstLetter(string) {
+    var matches = string.match(/\b(\w)/g);
+    return matches.join('');
+}
+
 function nameTemplate(fullName) {
+    let firstName = fullName.fname;
+    let lastName = fullName.lname;
+    let name = firstName.concat(" "+lastName);
+
     return `
-        <article class="fullName">
-            ${fullName.fname} + ${fullName.lname}
-            <p><strong>${fullName.category}</strong></p>
+        <article class="card">
+            <div class="avatar">${getFirstLetter(name)}<div>
+            <p class="fullName"> ${firstName} ${lastName} </p>
+            <p>${fullName.category}</p>
         </article>
     `
 }
 
+function getCategories(data) {
+    const catArray = [];
+    let categories = data.filter((element, index)=>{
+        catArray.push(element.category);
+        return catArray.indexOf(element.category) === index;
+    })
+    return categories;
+}
+
+function categoryTemplate(cat) {
+    return `
+    <div class="col-cat">
+        <button class="btn btn-cat">${cat.category}</button>
+    </div>
+    `
+}
 
 function appendData(data) {
-    let mainDiv = document.getElementById("namesList");
+    let mainDiv = document.getElementById("namesContainer");
+    let categories = getCategories(data);
     mainDiv.innerHTML = `
+    <section class="categoriesList">
+        <div class="row">${categories.map(categoryTemplate).join("")}</div>
+    </section>
+    <section class="namesList" 
         ${data.map(nameTemplate).join("")}
-        <p>these ${data.length} were listed</p>
+    </section>    
     `
 };
